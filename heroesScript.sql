194b2ece2da04f744dc6e4ad74323b37cfcfa5fa
diegoa915 <82744345+diegoa915@users.noreply.github.com>
2022-09-07 01:01:29 -0500

    Add files via upload

---
+++ heroesScript.sql
@@ -0,0 +1,12 @@
+create database heroes
+use heroes
+
+select * from mutantes
+
+
+INSERT INTO `heroes`.`mutantes` (`id_persona`, `condicion`, `grupo`, `location`, `name`, `poder`) VALUES ('1', 'Libertad', 'Heroe', 'Tierra', 'Superman', 'Super Fuerza');
+INSERT INTO `heroes`.`mutantes` (`id_persona`, `condicion`, `grupo`, `location`, `name`, `poder`) VALUES ('2', 'Libertad', 'Heroe', 'Asgard', 'Thor', 'Rayo');
+INSERT INTO `heroes`.`mutantes` (`id_persona`, `condicion`, `grupo`, `location`, `name`, `poder`) VALUES ('3', 'Detenido', 'Villano', 'Asgard', 'Loki', 'Magia');
+INSERT INTO `heroes`.`mutantes` (`id_persona`, `condicion`, `grupo`, `location`, `name`, `poder`) VALUES ('4', 'Desconocido', 'Heroe', 'Tierra', 'Capitan America', 'Super Fuerza');
+INSERT INTO `heroes`.`mutantes` (`id_persona`, `condicion`, `grupo`, `location`, `name`, `poder`) VALUES ('5', 'Detenido', 'Villano', 'Tierra', 'Magneto', 'Magnetismo');
+INSERT INTO `heroes`.`mutantes` (`id_persona`, `condicion`, `grupo`, `location`, `name`, `poder`) VALUES ('6', 'Desconocido', 'Villano', 'Desconocido', 'Galactus', 'Energia cosmica');
